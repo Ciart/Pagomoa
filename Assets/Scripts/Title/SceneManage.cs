3a8edf4c92e06d2adb9b0dba21565dbbee14c4cb
GameNight <tmddus9908@naver.com>
2023-08-23 12:33:16 +0900

    옵션

---
+++ Assets/Scripts/Title/SceneManage.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    public void StartScene()
+    {
+        SceneManager.LoadScene("Scenes/ItemTypeSample");
+    }
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
