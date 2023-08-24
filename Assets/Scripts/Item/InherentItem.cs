6afb5c1883c54d59a436c322d7b526af2c6df49e
SoExDobin <a90189829@gmail.com>
2023-08-24 14:14:56 +0900

    리모컨 작업중

---
+++ Assets/Scripts/Item/InherentItem.cs
@@ -0,0 +1,15 @@
+﻿using Player;
+using System.Collections;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "New Item", menuName = "New Item/Inherent item")]
+
+public class InherentItem : Item
+{
+    public override void Active(Status stat)
+    {
+        
+    }
+}
