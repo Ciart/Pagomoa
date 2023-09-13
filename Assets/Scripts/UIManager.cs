49018f13f36a64dadb6440ab0214e19f364572a3
GameNight <tmddus9908@naver.com>
2023-09-13 21:47:33 +0900

    인벤토리UI 교체 퀵슬롯 ì½ ,사용 코드 위치 변경, 폭탄 폭발 ì 애니메이션 추가

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -29,53 +29,14 @@ public class UIManager : MonoBehaviour
 
         playerInput = player.GetComponent<PlayerInput>();
 
-        playerInput.Actions.Slot1.started += context => {ControlQuickSlot(0);};
-        playerInput.Actions.Slot2.started += context => {ControlQuickSlot(1);};
-        playerInput.Actions.Slot3.started += context => {ControlQuickSlot(2);};
-        playerInput.Actions.Slot4.started += context => {ControlQuickSlot(3);};
-        playerInput.Actions.Slot5.started += context => {ControlQuickSlot(4);};
-        playerInput.Actions.Slot6.started += context => {ControlQuickSlot(5);};
-
         playerInput.Actions.SetEscUI.started += context => 
         {
-            bool activeEscUI = false;
-            if (EscUI.activeSelf == false)
-                activeEscUI = !activeEscUI;
-            EscUI.SetActive(activeEscUI);
+            SetEscUI();
         };
 
         playerInput.Actions.SetInventoryUI.started += context =>
         {
-            ActiveInventory = !ActiveInventory;
-            InventoryUI.SetActive(ActiveInventory);
-            if (InventoryUI.activeSelf == false)
-                HoverEvent.Instance.image.SetActive(ActiveInventory);
-        };
-        playerInput.Actions.UseQuickSlot.started += context =>
-        {
-            if (QuickSlotItemDB.instance.selectedSlot == null || QuickSlotItemDB.instance.selectedSlot.inventoryItem.item == null)
-                return;
-
-            if (QuickSlotItemDB.instance.selectedSlot.inventoryItem.item.itemType == Item.ItemType.Use)
-            {
-                QuickSlotItemDB.instance.selectedSlot.inventoryItem.count -= 1;
-                QuickSlotItemDB.instance.selectedSlot.SetItemCount();
-                QuickSlotItemDB.instance.selectedSlot.UseItem();
-
-                if (QuickSlotItemDB.instance.selectedSlot.inventoryItem.count == 0)
-                {
-                    InventoryDB.Instance.items.Remove(QuickSlotItemDB.instance.selectedSlot.inventoryItem);
-                    QuickSlotItemDB.instance.selectedSlot.SetSlotNull();
-                }
-                EtcInventory.Instance.UpdateSlot();
-            }
-            else if (QuickSlotItemDB.instance.selectedSlot.inventoryItem.item.itemType == Item.ItemType.Inherent)
-            {
-                QuickSlotItemDB.instance.selectedSlot.UseItem();
-                
-                QuickSlotItemDB.instance.selectedSlot.inventory.UpdateSlot();
-            } else
-                return;
+            SetInventoryUI();
         };
     }
     public void UpdateOxygenBar(float current_oxygen, float max_oxygen)
@@ -96,29 +57,22 @@ public class UIManager : MonoBehaviour
         digbar.fillAmount = holdtime / digtime;
         digbar.transform.parent.gameObject.SetActive(true);
     }
-    private void ControlQuickSlot(int n)
+    private void SetEscUI()
     {
-        for (int index = 0; index < QuickSlotItemDB.instance.quickSlots.Count; index++)
-        {
-            if (n == index)
-            {
-                if (QuickSlotItemDB.instance.selectedSlot != QuickSlotItemDB.instance.quickSlots[index])
-                {
-                    QuickSlotItemDB.instance.selectedSlot = QuickSlotItemDB.instance.quickSlots[index];
-                    QuickSlotItemDB.instance.quickSlots[index].selectedSlotImage.gameObject.SetActive(true);
-                    QuickSlotItemDB.instance.selectedSlot.transform.SetAsLastSibling();
-                }
-                else
-                {
-                    QuickSlotItemDB.instance.quickSlots[index].selectedSlotImage.gameObject.SetActive(false);
-                    QuickSlotItemDB.instance.selectedSlot = null;
-                }
-            }
-            else
-                QuickSlotItemDB.instance.quickSlots[index].selectedSlotImage.gameObject.SetActive(false);
-        }
-
+        bool activeEscUI = false;
+        if (EscUI.activeSelf == false)
+            activeEscUI = !activeEscUI;
+        EscUI.SetActive(activeEscUI);
     }
+    private void SetInventoryUI()
+    {
+        ActiveInventory = !ActiveInventory;
+        InventoryUI.SetActive(ActiveInventory);
+        if (InventoryUI.activeSelf == false)
+            HoverEvent.Instance.image.SetActive(ActiveInventory);
+    }
+
+    //}
 
 }
 
