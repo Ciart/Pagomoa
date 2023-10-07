6c15f3c0c437acaef6c7fdd8a8aeaa8f8cb5c585
Pdom <pdom0327@gmail.com>
2023-10-07 17:12:48 +0900

    Update prefab 화

--- Assets/Scripts/Player/PlayerDigger.cs
+++ Assets/Scripts/Player/PlayerDigger.cs
@@ -12,8 +12,7 @@ namespace Player
     [RequireComponent(typeof(PlayerController))]
     public class PlayerDigger : MonoBehaviour
     {
-        [HideInInspector]
-        public UnityEvent<float, float> DiggingEvent;
+        [HideInInspector] public UnityEvent<float, float> DiggingEvent;
         public UnityEvent digEndEvent;
 
         public Direction direction;
@@ -26,16 +25,19 @@ namespace Player
         public int width = 2;
         public int length = 1;
 
-        public TargetBrickChecker target;
+        public TargetBrickChecker targetPrefabs;
 
         private int[] drillSpeed = { 10, 20, 40, 80, 200, 5000, 1000000 };
         private int[] drillTierSetting = { 1, 2, 3, 4, 5 };
         
+        private TargetBrickChecker _target;
+
         private Status _status;
 
         private void Awake()
         {
             _status = GetComponent<Status>();
+            _target = Instantiate(targetPrefabs);
         }
 
         public void DrillUprade()
@@ -49,6 +51,7 @@ namespace Player
             if (drillLevel < drillTierSetting.Length)
                 drillTier = drillTierSetting[drillLevel];
         }
+
         private void Update()
         {
             if (!isDig)
@@ -56,9 +59,10 @@ namespace Player
                 return;
             }
 
-            target.ChangeTarget(DirectionCheck(width % 2 == 0), width, length, direction is Direction.Left or Direction.Right);
+            _target.ChangeTarget(DirectionCheck(width % 2 == 0), width, length,
+                direction is Direction.Left or Direction.Right);
 
-            foreach (var (x, y) in target.targetCoordsList)
+            foreach (var (x, y) in _target.targetCoordsList)
             {
                 var worldManager = WorldManager.instance;
                 worldManager.DigGround(new BrickCoords(x, y), _status.digSpeed);
@@ -87,72 +91,5 @@ namespace Player
 
             return WorldManager.ComputeCoords(transform.position + digVec);
         }
-
-        // void ICanDig()
-        // {
-        //     digEndEvent.Invoke();
-        //     _canDig = true;
-        //     _charging = 0;
-        // }
-        //
-        // IEnumerator PA(Vector2Int point1, Vector2Int point2)
-        // {
-        //     _canDig = false;
-        //
-        //     var worldManager = WorldManager.instance;
-        //     var tile1 = worldManager.world.GetBrick(point1.x, point1.y, out _)?.ground;
-        //     var tile2 = worldManager.world.GetBrick(point2.x, point2.y, out _)?.ground;
-        //
-        //     bool breakable1 = false, breakable2 = false;
-        //
-        //     breakable1 = tile1 ? worldManager.CheckBreakable(point1.x, point1.y, drillTier, "drill") : false;
-        //     breakable2 = tile2 ? worldManager.CheckBreakable(point1.x, point1.y, drillTier, "drill") : false;
-        //
-        //     if (breakable1 || breakable2)
-        //     {
-        //         float time1 = 0, time2 = 0, totalHungry = 0;
-        //
-        //         if (breakable1)
-        //         {
-        //             time1 = tile1.strength / _status.digSpeed;
-        //             totalHungry += 0.5f;
-        //         }
-        //         if (breakable2)
-        //         {
-        //             time2 = tile2.strength / _status.digSpeed;
-        //             totalHungry += 0.5f;
-        //         }
-        //
-        //         if (GetComponent<PlayerController>().Hungry(totalHungry))
-        //         {
-        //             time1 *= tiredDigSpeedScoop;
-        //             time2 *= tiredDigSpeedScoop;
-        //         }
-        //
-        //
-        //         Vector2Int currentPos1 = point1, currentPos2 = point2;
-        //         while (isDig && (time1 > _charging || time2 > _charging))
-        //         {
-        //             currentPos1 = DirectionCheck();
-        //             currentPos2 = DirectionCheck();
-        //
-        //             _charging += Time.fixedDeltaTime;
-        //             float time = time1 >= time2 ? time1 : time2;
-        //             DiggingEvent.Invoke(_charging, time);
-        //
-        //             yield return new WaitForSeconds(Time.fixedDeltaTime);
-        //             // 파던 위치가 달라지면 초기화 및 탈출
-        //             if ((currentPos1 != point1) || (currentPos2 != point2))
-        //             {
-        //                 ICanDig();
-        //                 yield break;
-        //             }
-        //         }
-        //         if (time1 <= _charging) worldManager.BreakGround(point1.x, point1.y, drillTier);
-        //         if (time2 <= _charging) worldManager.BreakGround(point2.x, point2.y, drillTier);
-        //     }
-        //
-        //     ICanDig();
-        // }
     }
 }
