9d694ed7e0fa44adda10dc03d4787a471954b50c
Pdom <pdom0327@gmail.com>
2023-12-02 18:24:13 +0900

    feat: ui

--- Assets/Scripts/UIs/MinimapUI.cs
+++ Assets/Scripts/UIs/MinimapUI.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -7,5 +8,27 @@ namespace UIs
     {
         public TextMeshProUGUI coordXText;
         public TextMeshProUGUI coordYText;
+
+        private Transform _player;
+        
+        private void Start()
+        {
+            _player = GameManager.instance.player.transform;
+        }
+
+        private void Update()
+        {
+            var position = _player.position;
+
+            if (Math.Round(position.x) >= 1)
+                coordXText.text = $"{position.x:+0}";
+            else if(position.x < 1)
+                coordXText.text = $"{position.x:0}";
+            
+            if(Math.Round(position.y) >= 1)
+                coordYText.text = $"{position.y:+0}";
+            else if(position.y < 1)
+                coordYText.text = $"{position.y:0}";
+        }
     }
 }
