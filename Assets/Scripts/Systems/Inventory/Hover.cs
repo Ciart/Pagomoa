f59b7e59dbb9ede10abad33f92f7ab96b2965ee2
Pdom <pdom0327@gmail.com>
2024-08-23 23:20:35 +0900

    wip: quest ui

--- Assets/Scripts/Systems/Inventory/Hover.cs
+++ Assets/Scripts/Systems/Inventory/Hover.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -8,12 +9,19 @@ namespace Ciart.Pagomoa.Systems.Inventory
     {
         [SerializeField] public Sprite[] hoverImage;
         [SerializeField] public Image boostImage;
-
+        
+        private void OnEnable()
+        {
+            boostImage.sprite = hoverImage[1];
+            InventoryUI.Instance.hoverSlot = null;
+        }
+        
         public virtual void OnPointerEnter(PointerEventData eventData)
         {
             boostImage.sprite = hoverImage[0];
             InventoryUI.Instance.hoverSlot = this.gameObject.GetComponent<InventorySlotUI>();
         }
+        
         public virtual void OnPointerExit(PointerEventData eventData)
         {
             boostImage.sprite = hoverImage[1];
