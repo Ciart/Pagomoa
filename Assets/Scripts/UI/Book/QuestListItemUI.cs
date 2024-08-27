6f2ce0a71aecc33483df5b59b9c317476027f25d
Pdom <pdom0327@gmail.com>
2024-08-27 22:15:33 +0900

    wip

---
+++ Assets/Scripts/UI/Book/QuestListItemUI.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ciart.Pagomoa.UI.Book
+{
+    public class QuestListItemUI: MonoBehaviour
+    {
+        public Image progressBar;
+        public TextMeshProUGUI titleText;
+    }
+}
