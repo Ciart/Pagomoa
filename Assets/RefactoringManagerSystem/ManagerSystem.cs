f123431d3a7870d0df3a185f7d3000f3c872b565
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2024-10-12 22:19:16 +0900

    PManager 선언

---
+++ Assets/RefactoringManagerSystem/ManagerSystem.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PGameInstance : MonoBehaviour
+{
+    private static PGameInstance instance = null;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        if(Managers == null)
+        {
+            Managers = new List<PManager>();
+
+            foreach(System.Reflection.Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (assembly.GetType().IsSubclassOf(typeof(PManager)))
+                {
+                    PManager manager = Activator.CreateInstance(assembly.GetType()) as PManager;
+                    Managers.Add(manager);
+                }
+            }
+        }
+
+        foreach (PManager mgr in Managers)
+        {
+            mgr.PreAwake();
+        }
+
+        foreach (PManager mgr in Managers)
+        {
+            mgr.Awake();
+        }
+
+        foreach (PManager mgr in Managers)
+        {
+            mgr.PostAwake();
+        }
+    }
+
+
+   private void Start()
+   {
+        if (Managers != null)
+        {
+            foreach (PManager manager in Managers)
+            {
+                manager.Start();
+            }
+        }
+    }
+
+   // Update is called once per frame
+   private void Update()
+   {
+        if(Managers != null)
+        {
+            foreach (PManager manager in Managers)
+            {
+                if(manager.GetNeedToPreUpdate())
+                {
+                    manager.PreUpdate();
+                }
+            }
+         
+            foreach (PManager manager in Managers) 
+            {
+                if(manager.GetNeedToUpdate())
+                {
+                    manager.Update();
+                }
+            }
+
+            foreach (PManager manager in Managers)
+            { 
+                if(manager.GetNeedToPostUpdate())
+                {
+                    manager.PostUpdate();
+                }
+            }
+        }
+   }
+
+    private void FixedUpdate()
+    {
+        if (Managers != null)
+        {
+             foreach (PManager manager in Managers)
+             {
+                if(manager.GetNeedToPreFixedUpdate())
+                {
+                    manager.PreFixedUpdate();
+                }
+             }
+             
+             foreach (PManager manager in Managers)
+             {
+                if(manager.GetNeedToFixedUpdate())
+                {
+                    manager.FixedUpdate();
+                }
+
+             }
+
+             foreach (PManager manager in Managers) 
+             {
+                if(manager.GetNeedToPostFixedUpdate())
+                {
+                    manager.PostFixedUpdate();
+                }
+             }
+        }
+    }
+
+    private List<PManager> Managers = null;
+}
---
+++ Assets/RefactoringManagerSystem/PManager.cs
@@ -0,0 +1,31 @@
+class PManager
+{
+    public virtual void PreAwake() {}
+    public virtual void Awake() { }
+    public virtual void PostAwake() {}
+    public virtual void Start() { }
+
+    private bool NeedToPreUpdate = false;
+    private bool NeedToUpdate = false;
+    private bool NeedToPostUpdate = false;
+
+    private bool NeedToPreFixedUpdate = false;
+    private bool NeedToFixedUpdate = false;
+    private bool NeedToPostFixedUpdate = false;
+
+    public bool GetNeedToPreUpdate() {  return NeedToPreUpdate; }
+    public bool GetNeedToUpdate() { return NeedToUpdate; }
+    public bool GetNeedToPostUpdate() { return NeedToPostUpdate; }
+
+    public bool GetNeedToPreFixedUpdate() {  return NeedToPreFixedUpdate; }
+    public bool GetNeedToFixedUpdate() { return NeedToFixedUpdate; }
+    public bool GetNeedToPostFixedUpdate() { return NeedToPostFixedUpdate; }
+
+    public virtual void PreUpdate() { }
+    public virtual void Update() { }
+    public virtual void PostUpdate() { }
+    public virtual void PreFixedUpdate () { }
+    public virtual void FixedUpdate() { }
+    public virtual void PostFixedUpdate() { }
+    public virtual void OnDestroy() { }
+}
