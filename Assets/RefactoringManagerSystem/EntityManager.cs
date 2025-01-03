0954cfa9e8c479447773abe08f5f68f0d7249dd7
차경훈 <cksuwjr@naver.com>
2025-01-03 14:31:38 +0900

    안개, 플레이어 땅짚고 오르기 중력, 콘솔 오류 버그들 수정

--- Assets/RefactoringManagerSystem/EntityManager.cs
+++ Assets/RefactoringManagerSystem/EntityManager.cs
@@ -43,7 +43,7 @@ namespace Ciart.Pagomoa.Entities
         public void Despawn(EntityController controller)
         {
             _entities.Remove(controller);
-            
+            if (controller is null) return;
             Object.Destroy(controller.gameObject);
         }
 
