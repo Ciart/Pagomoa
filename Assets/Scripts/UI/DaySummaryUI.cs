4ca443fbe2f4c9a56a43e2835d8742b3e2fc8dbc
Pdom <pdom0327@gmail.com>
2025-03-12 20:45:13 +0900

    feat: 날 넘어갈 때 날짜 표기

--- Assets/Scripts/UI/DaySummaryUI.cs
+++ Assets/Scripts/UI/DaySummaryUI.cs
@@ -1,9 +1,18 @@
 using Ciart.Pagomoa.Systems;
+using TMPro;
 using UnityEngine;
 
 public class DaySummaryUI : MonoBehaviour {
+    public TextMeshProUGUI dateText;
+
     public void OnSubmit() {
-        Game.instance.MoveToNextDay();
+        Game.Instance.MoveToNextDay();
         gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        var date = Game.Instance.Time.date;
+        dateText.text = $"{date}일차 종료";
     }
 }
