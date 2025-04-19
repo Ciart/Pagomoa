76eff122e64a817909c4a2f8624335d5c00100c4
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-04-19 21:21:33 +0900

    feat:최소 해상도 조정 추가 및 컴파일용 에디터 코드 블럭 수정 #289

--- Assets/Scripts/UI/ScaleUI.cs
+++ Assets/Scripts/UI/ScaleUI.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+
 namespace Ciart.Pagomoa.UI
 {
     public class UIScaler : MonoBehaviour
     {
-        public int baseHeight = 360;
-        public int baseWidth = 640;
+        const int BaseHeight = 360;
+        const int BaseWidth = 640;
         public int widestElement = 510;
-        private RectTransform rectTransform;
         private CanvasScaler scaler;
         private int[] res = new int[2];
         private int newheight = 0;
@@ -17,28 +17,34 @@ namespace Ciart.Pagomoa.UI
         private void Start()
         {
             scaler = GetComponent<CanvasScaler>();
-            rectTransform = GetComponent<RectTransform>();
             ScaleUI();
         }
     
         private void LateUpdate()
         {
-          
+#if !UNITY_EDITOR
+            if (Screen.width < BaseWidth || Screen.height < BaseHeight)
+            {
+                Screen.SetResolution(BaseWidth, BaseHeight, FullScreenMode.Windowed);
+            }
+#endif
+            
             if (res[0] != Screen.height || res[1] != Screen.width)
             {
-                Debug.Log("Scale Changed");
+                //Debug.Log("Scale Changed");
                 ScaleUI();
             }
-    
+            
+            
         }
     
         private void ScaleUI()
         {
-            scaler.scaleFactor = Mathf.RoundToInt((float)Screen.height / baseHeight);
+            scaler.scaleFactor = Mathf.RoundToInt((float)Screen.height / BaseHeight);
     
             if (widestElement * scaler.scaleFactor > Screen.width)
             {
-                scaler.scaleFactor = Mathf.RoundToInt((float)Screen.width / baseWidth);
+                scaler.scaleFactor = Mathf.RoundToInt((float)Screen.width / BaseWidth);
             }
           
             res[0] = Screen.height;
